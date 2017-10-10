581c7a0067175af2a94491e7e3404c5c50020909
SamTheSoftwareGeek <30541255+SamTheSoftwareGeek@users.noreply.github.com>
2017-10-09 20:52:11 -0400

    Implemented the Verlet integration

--- projectile.cpp
+++ projectile.cpp
@@ -7,30 +7,20 @@ const double dt = 0.001; // change in time
 const double q = 1; // charge
 const double e0 = 1; // electric field
 const double k = 0.5; // constant 
-const double w = 1; // period 
-
-
-double electricForceXComponent (TState s){
-  return q * e0 * cos(k * s.position.z - w * s.t) / sqrt(2);   
-}
-
-double electricForceYComponent (TState s){
-  return q * e0 * sin(k * s.position.z - w * s.t) / sqrt(2);   
-}
-
-double electricForceZComponent (TState s){
-  return q * 0;   
-}
+const double w = 2; // period 
 
 auto force(TState s) {
-  return VecR3<double>{electricForceXComponent(s), electricForceYComponent (s), electricForceZComponent (s)};
+  return VecR3<double>{e0 * std::cos(k * s.position.z - w * s.t) / sqrt(2),
+    e0 * std::sin(k * s.position.z - w * s.t) / sqrt(2),
+    0} * q;
  }
 
-auto euler_step(TState s, VecR3<double> accel) {
+auto verlet_step(TState currentState, VecR3<double> accel) {
   TState next;
-  next.t = s.t + dt;
-  next.position = s.position + (s.velocity * dt);
-  next.velocity = s.velocity + (accel * dt);
+  next.t = currentState.t + dt;
+  next.position = currentState.position + (currentState.velocity)*dt + 0.5*accel*dt*dt;
+  VecR3<double> nextAccel = force(next)/m;
+  next.velocity = currentState.velocity + 0.5*(accel + nextAccel)*dt;
   return next;
 }
 
@@ -41,13 +31,15 @@ void n_steps(unsigned n, TState state0) {
   else {
     auto state = state0;
     for (unsigned k = 0; k < n; ++k) {
-      state = euler_step(state, force(state) / m);
+      state = verlet_step(state, force(state) / m);
       print_tstate(state);
     }
   }
 }
 
 int main() {
-  n_steps(12000, TState{0., {-e0 / (w * w * sqrt(2)), 0, 0}, {0, e0 / (w * sqrt(2)), -0.25}});
+  n_steps(12000, TState{0.,
+    {-e0 / (w * w * sqrt(2)), 0, 0},
+    {0, e0 / (w * sqrt(2)), -.25}});
   return 0;
 }
